e7afcb448505b32d1aa13bb3278762ab1792a324
Mr.Puttipong Doungvichai <puttipong.dou@gmail.com>
2023-07-22 22:30:43 +0700

    simple

--- arduino.ino
+++ arduino.ino
@@ -1,18 +1,21 @@
-int count = 0;
+#include <Servo.h>
+Servo servoMotor;
+
+const int servoPin = 13;  // Define the pin that the LED is connected to.
 
 void setup() {
-  // Initialize the serial port
-  Serial.begin(9600);
+  servoMotor.attach(servoPin);
 }
 
 void loop() {
-  // Print "Hello, world!" to the serial port
-  if (count < 10) {
-    Serial.println(String(count) + ": Hello, world! Arduino");
-    count++;
-  } else {
-    // Delay for 10 seconds.
-    delay(10000);
-    count = 0;
-  }
+  servoRotate();
+}
+
+void servoRotate() {
+  servoMotor.write(0);
+  delay(1000);            // Delay for 1 second
+  servoMotor.write(90);   // Rotate the servo to 90 degrees (center angle)
+  delay(1000);            // Delay for 1 second
+  servoMotor.write(180);  // Rotate the servo to 180 degrees (maximum angle)
+  delay(1000);            // Delay for 1 second
 }
